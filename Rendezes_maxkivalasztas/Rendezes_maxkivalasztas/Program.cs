a34facc078123db0e8105d42c2d0c0c472b43380
Séllei Zsigmond <31158052+Zsigi0219@users.noreply.github.com>
2019-07-29 18:22:36 +0200

    Add files via upload

---
+++ Rendezes_maxkivalasztas/Rendezes_maxkivalasztas/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rendezes_maxkivalasztas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] t = { 8, 3, 9, 1, 5, 2, 7 };
+            int n = t.Length;
+
+            for (int i=0; i<n; i++)
+                Console.Write(t[i] + " ");
+            Console.WriteLine();
+
+            for (int i = n-1; i > 0; i--)
+            {
+                int max = i;
+                for (int j = 0; j <= i; j++)
+                    if (t[j] > t[max])
+                        max = j;
+                int swap = t[i];
+                t[i] = t[max];
+                t[max] = swap;
+                
+            }
+            for (int i = 0; i < n; i++)
+                Console.Write(t[i] + " ");
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
