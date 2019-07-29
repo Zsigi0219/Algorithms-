97ccd1719ba6fc93bfe6cf7e35db2af9677f6273
Séllei Zsigmond <31158052+Zsigi0219@users.noreply.github.com>
2019-07-29 18:21:25 +0200

    Add files via upload

---
+++ Osszegzes_Algoritmus/Osszegzes_Algoritmus/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Osszegzes_Algoritmus
+{
+    class Program
+    {
+        //Az algoritmus a tömbben található számok összegét fogja kiírni
+        static void Main(string[] args)
+        {
+
+            int[] t = { 9, 7, 3, 5, 4, 2, 6 };
+            int n = t.Length;
+            int c = 0;
+
+            for (int i = 0; i < n; i++)
+                if (t[i] < 5)
+                    c++;
+            Console.WriteLine("5 nél kisebb számok száma {0}",c);
+            
+            
+            
+            //int[] tomb = { 8, 9, 5, 4, 1, 6};
+            //int n = 6;
+            //Console.WriteLine("Összegzés tétel");
+            //
+            //int osszeg = 0;
+            //for (int i = 0; i < n; i++)
+            //    osszeg = osszeg + tomb[i];
+            //Console.WriteLine("Összeg: "+ osszeg);
+            
+
+            //int[] tomb = { 8, 9, 5, 4, 1 };
+            //int n = 5;
+            //Console.WriteLine("Összegzés tétel");
+            //int osszeg = 0;
+            //for (int i = 0; i < n; i++)
+            //    osszeg = osszeg + tomb[i];
+            //Console.WriteLine("Összeg: " + osszeg);
+            _ = Console.ReadKey();
+        }
+    }
+}
