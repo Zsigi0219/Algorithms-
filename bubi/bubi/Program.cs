9c01b0e117f149f9fc38856a97e6338d6b65792c
Séllei Zsigmond <31158052+Zsigi0219@users.noreply.github.com>
2019-07-29 18:18:43 +0200

    Add files via upload

---
+++ bubi/bubi/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bubi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] t = new int[] { 5, 4, 9, 3, 7};
+            int n = t.Length;
+
+            for (int i = 0; i < n; i++)
+                Console.Write("{0},", t[i]);
+            Console.WriteLine();
+
+            //Buborék rendezés
+            for (int i = n-1; i > 0; i--)
+                for (int j = 0; j < i; j++)
+                    if (t[j] > t[j+1])
+                    {
+                        int tmp = t[j + 1];
+                        t[j + 1] = t[j];
+                        t[j] = tmp;
+                    }
+            //kiíratás rendezés után
+            for (int i = 0; i < n; i++)
+                Console.Write("{0},",t[i]);
+            Console.WriteLine();
+            Console.ReadLine();           
+        }
+    }
+}
